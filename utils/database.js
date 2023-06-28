f08c9ce1ea39bbc02ebf0968f8df5160274a0ea4
João Victor <johnnywinchest3r@gmail.com>
2023-06-28 09:00:41 -0300

    second commit

--- utils/database.js
+++ utils/database.js
@@ -15,6 +15,7 @@ export const connectToDB = async ()=> {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: 'share_prompt',
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
