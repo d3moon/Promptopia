8ee5b3a96b9eb854ff9ae6dc0f862e86100b52dc
João Victor <johnnywinchest3r@gmail.com>
2023-06-28 08:19:09 -0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google'
+import dotenv from 'dotenv'
+import { connectToDB } from "@utils/database";
+import User from '@models/user'
+
+dotenv.config()
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ],
+  callbacks: {
+
+    async session({session}) {
+      const sessionUser = await User.findOne({email: session.user.email})
+  
+      session.user.id = sessionUser._id.toString()
+      return session
+    },
+  
+    async signIn({profile}) {
+      try {
+        await connectToDB()
+  
+        const userExists = await User.findOne({email: profile.email})
+  
+  
+        console.log(profile.email)
+        if(!userExists) {
+         await User.create({
+          email: profile.email,
+          username: profile.name
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .replace(/~/g, "")
+          .replace(" ", "")
+          .toLowerCase(),
+          image: profile.picture
+         })
+        }
+  
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      } 
+    }
+  }
+})
+
+
+export {handler as GET, handler as POST}
---
+++ components/Feed.jsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import PromptCard from "@components/PromptCard"
+
+
+const PromptCardList = ({data, handleTagClick}) => {
+  console.log(data)
+  return (
+    <div className="mt-16 prompt-layout">
+      {
+        data.map((post)=> (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick} 
+          />
+        ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+
+  const [posts, setPosts] = useState([])
+  
+  //Search states
+  const [searchText, setSearchText] = useState("")
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
+
+  
+  useEffect( ()=>{
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      setPosts(data)
+    }
+    fetchPosts()
+  }, [])
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i")
+    return posts.filter((item)=> 
+      regex.test(item.creator.username) ||
+      regex.test(item.tag) ||
+      regex.test(item.prompt)      
+    )
+  }
+  
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchedResults(searchResult)
+      }, 500)
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
+
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+        type="text"
+        placeholder="Search for a tag or an username"
+        value={searchText}
+        onChange={handleSearchChange}
+        className="search_input peer"
+        required
+         />
+      </form>
+
+      {searchText ? (
+        <PromptCardList
+        data={searchedResults}
+        handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
+    
+    </section>
+  )
+}
+
+export default Feed
